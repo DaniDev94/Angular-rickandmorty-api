139bc95567b147452fa22ca910f475cc20bb5638
Daniel López <daniel.lp1094@gmail.com>
2021-10-21 20:46:14 +0200

    FEAT: main menu and navigation component implemented

---
+++ src/app/core/components/menu-nav/menu-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-nav',
+  templateUrl: './menu-nav.component.html',
+  styleUrls: ['./menu-nav.component.scss']
+})
+export class MenuNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
